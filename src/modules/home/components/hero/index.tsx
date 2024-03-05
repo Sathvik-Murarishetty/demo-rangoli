528f07e67a852e2b3891bac2871565846832534e
Sathvik-Murarishetty <sathvikmurarishetty18@gmail.com>
2024-03-05 13:21:22 +0530

    first commit

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,33 +1,56 @@
+"use client";
+
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import { BiArrowBack } from "react-icons/bi"
+import Image from "next/image";
+import React, { useCallback } from "react";
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
 
 const Hero = () => {
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay( { delay: 3000 } )] )
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subt">
+      <div className='embla mx-auto mt-12 max-w-lg'>
+        <div className='embla__viewport h-56 border' ref={emblaRef}>
+          <div className='embla__container h-full'>
+            <div className='embla__slide flex items-center justify-center'>
+              Slide 1
+            </div>
+            <div className='embla__slide flex items-center justify-center'>
+              Slide 2
+            </div>
+            <div className='embla__slide flex items-center justify-center'>
+              Slide 3
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-3 flex justify-between'>
+          <button
+            className='w-20 bg-black px-2 py-1 text-sm text-white'
+            onClick={scrollPrev}
           >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            Prev
+          </button>
+          <button
+            className='w-20 bg-black px-2 py-1 text-sm text-white'
+            onClick={scrollNext}
           >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+            Next
+          </button>
+        </div>
       </div>
     </div>
   )
