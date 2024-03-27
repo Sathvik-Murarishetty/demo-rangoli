e80ff664970a39d61ba42c3cf1fa2d81641b253f
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-03-27 19:17:23 +0530

    Update index.tsx - add to cart button

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Text } from "@medusajs/ui"
 
 import { ProductPreviewType } from "types/global"
@@ -8,6 +10,20 @@ import { Region } from "@medusajs/medusa"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import Thumbnail from "../thumbnail"
 import PreviewPrice from "./price"
+
+import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
+import { Button } from "@medusajs/ui"
+import { isEqual } from "lodash"
+import { useParams } from "next/navigation"
+import { useEffect, useMemo, useRef, useState } from "react"
+
+import { useIntersection } from "@lib/hooks/use-in-view"
+import { addToCart } from "@modules/cart/actions"
+import Divider from "@modules/common/components/divider"
+import OptionSelect from "@modules/products/components/option-select"
+
+import MobileActions from "../mobile-actions"
+import ProductPrice from "../product-price"
 
 export default async function ProductPreview({
   productPreview,
@@ -32,6 +48,102 @@ export default async function ProductPreview({
     region,
   })
 
+  export default function ProductActions({
+  product,
+  region,
+}: ProductActionsProps) {
+  const [options, setOptions] = useState<Record<string, string>>({})
+  const [isAdding, setIsAdding] = useState(false)
+
+  const countryCode = useParams().countryCode as string
+
+  const variants = product.variants
+
+  // initialize the option state
+  useEffect(() => {
+    const optionObj: Record<string, string> = {}
+
+    for (const option of product.options || []) {
+      Object.assign(optionObj, { [option.id]: undefined })
+    }
+
+    setOptions(optionObj)
+  }, [product])
+
+  // memoized record of the product's variants
+  const variantRecord = useMemo(() => {
+    const map: Record<string, Record<string, string>> = {}
+
+    for (const variant of variants) {
+      if (!variant.options || !variant.id) continue
+
+      const temp: Record<string, string> = {}
+
+      for (const option of variant.options) {
+        temp[option.option_id] = option.value
+      }
+
+      map[variant.id] = temp
+    }
+
+    return map
+  }, [variants])
+
+  // memoized function to check if the current options are a valid variant
+  const variant = useMemo(() => {
+    let variantId: string | undefined = undefined
+
+    for (const key of Object.keys(variantRecord)) {
+      if (isEqual(variantRecord[key], options)) {
+        variantId = key
+      }
+    }
+
+    return variants.find((v) => v.id === variantId)
+  }, [options, variantRecord, variants])
+
+  // if product only has one variant, then select it
+  useEffect(() => {
+    if (variants.length === 1 && variants[0].id) {
+      setOptions(variantRecord[variants[0].id])
+    }
+  }, [variants, variantRecord])
+
+  // update the options when a variant is selected
+  const updateOptions = (update: Record<string, string>) => {
+    setOptions({ ...options, ...update })
+  }
+
+  // check if the selected variant is in stock
+  const inStock = useMemo(() => {
+    if (variant && !variant.inventory_quantity) {
+      return false
+    }
+
+    if (variant && variant.allow_backorder === false) {
+      return true
+    }
+  }, [variant])
+
+  const actionsRef = useRef<HTMLDivElement>(null)
+
+  const inView = useIntersection(actionsRef, "0px")
+
+  // add the selected variant to the cart
+  const handleAddToCart = async () => {
+    if (!variant?.id) return null
+
+    setIsAdding(true)
+
+    await addToCart({
+      variantId: variant.id,
+      quantity: 1,
+      countryCode,
+    })
+
+    setIsAdding(false)
+  }
+
   return (
     <LocalizedClientLink
       href={`/products/${productPreview.handle}`}
@@ -49,6 +161,30 @@ export default async function ProductPreview({
             {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
           </div>
         </div>
+        <Button
+          onClick={handleAddToCart}
+          disabled={!inStock || !variant}
+          variant="primary"
+          className="w-full h-10"
+          isLoading={isAdding}
+        >
+          {!variant
+            ? "Select variant"
+            : !inStock
+            ? "Out of stock"
+            : "Add to cart"}
+        </Button>
+        <MobileActions
+          product={product}
+          variant={variant}
+          region={region}
+          options={options}
+          updateOptions={updateOptions}
+          inStock={inStock}
+          handleAddToCart={handleAddToCart}
+          isAdding={isAdding}
+          show={!inView}
+        />
       </div>
     </LocalizedClientLink>
   )
