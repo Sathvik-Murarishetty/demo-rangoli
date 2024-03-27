e1ea2dc5b2fe08253ce93ecd82f553477f281e6a
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-03-27 19:26:55 +0530

    Update index.tsx - atc

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { Text } from "@medusajs/ui"
 
 import { ProductPreviewType } from "types/global"
@@ -7,23 +5,10 @@ import { ProductPreviewType } from "types/global"
 import { retrievePricedProductById } from "@lib/data"
 import { getProductPrice } from "@lib/util/get-product-price"
 import { Region } from "@medusajs/medusa"
+import { addToCart } from "@modules/cart/actions"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import Thumbnail from "../thumbnail"
 import PreviewPrice from "./price"
-
-import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
-import { Button } from "@medusajs/ui"
-import { isEqual } from "lodash"
-import { useParams } from "next/navigation"
-import { useEffect, useMemo, useRef, useState } from "react"
-
-import { useIntersection } from "@lib/hooks/use-in-view"
-import { addToCart } from "@modules/cart/actions"
-import Divider from "@modules/common/components/divider"
-import OptionSelect from "@modules/products/components/option-select"
-
-import MobileActions from "../mobile-actions"
-import ProductPrice from "../product-price"
 
 export default async function ProductPreview({
   productPreview,
@@ -48,102 +33,6 @@ export default async function ProductPreview({
     region,
   })
 
-  export default function ProductActions({
-  product,
-  region,
-}: ProductActionsProps) {
-  const [options, setOptions] = useState<Record<string, string>>({})
-  const [isAdding, setIsAdding] = useState(false)
-
-  const countryCode = useParams().countryCode as string
-
-  const variants = product.variants
-
-  // initialize the option state
-  useEffect(() => {
-    const optionObj: Record<string, string> = {}
-
-    for (const option of product.options || []) {
-      Object.assign(optionObj, { [option.id]: undefined })
-    }
-
-    setOptions(optionObj)
-  }, [product])
-
-  // memoized record of the product's variants
-  const variantRecord = useMemo(() => {
-    const map: Record<string, Record<string, string>> = {}
-
-    for (const variant of variants) {
-      if (!variant.options || !variant.id) continue
-
-      const temp: Record<string, string> = {}
-
-      for (const option of variant.options) {
-        temp[option.option_id] = option.value
-      }
-
-      map[variant.id] = temp
-    }
-
-    return map
-  }, [variants])
-
-  // memoized function to check if the current options are a valid variant
-  const variant = useMemo(() => {
-    let variantId: string | undefined = undefined
-
-    for (const key of Object.keys(variantRecord)) {
-      if (isEqual(variantRecord[key], options)) {
-        variantId = key
-      }
-    }
-
-    return variants.find((v) => v.id === variantId)
-  }, [options, variantRecord, variants])
-
-  // if product only has one variant, then select it
-  useEffect(() => {
-    if (variants.length === 1 && variants[0].id) {
-      setOptions(variantRecord[variants[0].id])
-    }
-  }, [variants, variantRecord])
-
-  // update the options when a variant is selected
-  const updateOptions = (update: Record<string, string>) => {
-    setOptions({ ...options, ...update })
-  }
-
-  // check if the selected variant is in stock
-  const inStock = useMemo(() => {
-    if (variant && !variant.inventory_quantity) {
-      return false
-    }
-
-    if (variant && variant.allow_backorder === false) {
-      return true
-    }
-  }, [variant])
-
-  const actionsRef = useRef<HTMLDivElement>(null)
-
-  const inView = useIntersection(actionsRef, "0px")
-
-  // add the selected variant to the cart
-  const handleAddToCart = async () => {
-    if (!variant?.id) return null
-
-    setIsAdding(true)
-
-    await addToCart({
-      variantId: variant.id,
-      quantity: 1,
-      countryCode,
-    })
-
-    setIsAdding(false)
-  }
-
   return (
     <LocalizedClientLink
       href={`/products/${productPreview.handle}`}
@@ -162,7 +51,7 @@ export default async function ProductPreview({
           </div>
         </div>
         <Button
-          onClick={handleAddToCart}
+          onClick={}
           disabled={!inStock || !variant}
           variant="primary"
           className="w-full h-10"
@@ -180,8 +69,8 @@ export default async function ProductPreview({
           region={region}
           options={options}
           updateOptions={updateOptions}
-          inStock={inStock}
-          handleAddToCart={handleAddToCart}
+          inStock={}
+          handleAddToCart={}
           isAdding={isAdding}
           show={!inView}
         />
