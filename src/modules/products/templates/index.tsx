b496db1e6fbad87449e13d6d63528457c10b38bf
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-03-30 12:18:17 +0530

    Update index.tsx - product page info

--- src/modules/products/templates/index.tsx
+++ src/modules/products/templates/index.tsx
@@ -32,7 +32,6 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
       <div className="content-container flex flex-col small:flex-row small:items-start py-6 relative">
         <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
           <ProductInfo product={product} />
-          <ProductTabs product={product} />
         </div>
         <div className="block w-full relative">
           <ImageGallery images={product?.images || []} />
