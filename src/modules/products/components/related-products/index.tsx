d7e7bf85485efaa3bf0e8a80b5ec7d3b98c8d0fa
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-03-30 12:12:33 +0530

    Update index.tsx - ralated products

--- src/modules/products/components/related-products/index.tsx
+++ src/modules/products/components/related-products/index.tsx
@@ -63,10 +63,10 @@ export default async function RelatedProducts({
   return (
     <div className="product-page-constraint">
       <div className="flex flex-col items-center text-center mb-16">
-        <span className="text-base-regular text-gray-600 mb-6">
+        <span className="text-2xl-regular text-gray-600 mb-6">
           Related products
         </span>
-        <p className="text-2xl-regular text-ui-fg-base max-w-lg">
+        <p className="text-xl-regular text-ui-fg-base max-w-lg">
           You might also want to check out these products.
         </p>
       </div>
