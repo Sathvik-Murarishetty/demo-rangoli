36423c5ef0b9b00f9758389c189b3d05476b681d
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-03 23:11:52 +0530

    Update index.tsx - original code

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -39,16 +39,14 @@ export default async function Nav() {
       </div>
       <Suspense
        fallback={
-        {/* Apply text-xl class to the div */}
+        <div className="text-xl"> {/* Apply text-xl class to the div */}
          <LocalizedClientLink
           className="hover:text-ui-fg-base flex gap-2"
           href="/cart"
          >
-        <div className="text-xl">
           Cart (0)
-         </div>
          </LocalizedClientLink>
-        
+        </div>
        }
       >
        <CartButton />
