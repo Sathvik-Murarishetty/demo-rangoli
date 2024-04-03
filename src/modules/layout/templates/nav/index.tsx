9fe034eccc5dad0fcf2da544eb5ce143ecabb8ca
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-03 22:25:00 +0530

    Update index.tsx - text header size

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -11,7 +11,7 @@ export default async function Nav() {
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 mx-auto border-b duration-200 bg-orange-400 text-2lg border-ui-border-base">
+      <header className="relative h-20 mx-auto border-b duration-200 bg-orange-400 text-xl border-ui-border-base">
         <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
