1242bd3721ab96b84d8d02b4f293e92fd4309675
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-06 21:49:10 +0530

    Update index.tsx - line

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -144,6 +144,11 @@ const [currentSlide, setCurrentSlide] = useState(0);
       </div>
     </div>
   </section>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <div style={{ position: 'relative', width: '70%', height: 'auto', overflow: 'hidden', marginBottom: '20px' }}>
+        <img src={"https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712420326/line_01_aqnpm4.png"} alt="------" style={{ width: '100%', height: '100%' }} />
+      </div>
+    </div>
 
   <div className="flex justify-center py-16">
     <h1 className="mt-4 text-5xl font-cormorant-garamond-bold text-gray-800">Our Best Sellers</h1>
