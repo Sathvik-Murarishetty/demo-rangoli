c62f7aaeafbfd0791dc068257d42f94d21e83cc7
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-07 11:31:56 +0530

    Update index.tsx - store page

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -19,14 +19,10 @@ const StoreTemplate = ({
 
   return (
     <div className="bg-orange-50">
-  <div className="flex justify-center py-10">
-    <div className="flex flex-col small:flex-row small:items-start py-6 content-container">
-      <RefinementList sortBy={sortBy || "created_at"} />
-      <div>
-  <div className="flex justify-center">
-    <h1 className="mt-4 text-5xl font-cormorant-garamond-bold text-gray-800 mb-5">Shop by Category</h1>
-  </div>
-  <section style={{ position: 'relative', width: '80%', height: 'auto', overflow: 'hidden', marginBottom: '20px', margin: '0 auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div className="flex justify-center py-10">
+    <h1 className="mt-4 text-5xl font-cormorant-garamond-bold text-gray-800">Categories</h1>
+    </div>
+<section style={{ position: 'relative', width: '80%', height: 'auto', overflow: 'hidden', marginBottom: '20px', margin: '0 auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
   <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-80 mx-auto md:w-full">
     <div className="relative aspect-w-1 aspect-h-1">
       <a href="/collections/sweets">
@@ -60,25 +56,7 @@ const StoreTemplate = ({
     </div>
   </div>
 </section>
-
-</div>
     </div>
-  </div>
-      <div className = "pl-20 pr-20">
-<div className="w-full mx-auto">
-    <div className="mb-8 text-2xl-semi">
-      <h1>All products</h1>
-    </div>
-    <Suspense fallback={<SkeletonProductGrid />}>
-      <PaginatedProducts
-        sortBy={sortBy || "created_at"}
-        page={pageNumber}
-        countryCode={countryCode}
-      />
-    </Suspense>
-  </div>
-        </div>
-</div>
   )
 }
 
