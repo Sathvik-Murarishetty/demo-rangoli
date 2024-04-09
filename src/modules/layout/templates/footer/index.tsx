f22f730a0716e13b83d5ad6b1a7ed5be31bf54f0
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-10 00:14:55 +0530

    Update index.tsx - footer rangoli address

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -10,7 +10,7 @@ export default async function Footer() {
   return (
     <footer className="border-t border-ui-border-base w-full bg-[#D35400]">
       <div className="content-container flex flex-col w-full">
-        <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between py-10">
+        <div className="flex flex-col gap-y-3 xsmall:flex-row items-start justify-between py-10">
           <div>
             <LocalizedClientLink
               href="/"
