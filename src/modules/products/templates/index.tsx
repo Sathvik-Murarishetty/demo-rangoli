1c5022103c5796b8d78dcc0e767c52e942014d6c
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-09 10:41:43 +0530

    Update index.tsx - product page bg colour

--- src/modules/products/templates/index.tsx
+++ src/modules/products/templates/index.tsx
@@ -29,7 +29,7 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
 
   return (
     <>
-      <div className="content-container flex flex-col small:flex-row small:items-start py-6 relative">
+      <div className="content-container flex flex-col bg-orange-50 small:flex-row small:items-start py-6 relative">
         <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
           <ProductInfo product={product} />
         </div>
