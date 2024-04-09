773e8d8b17f66eb031824275354bec6fd90a5e9a
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-10 02:09:32 +0530

    Update index.tsx - pl

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -22,7 +22,7 @@ export default async function Footer() {
                 <br />  1584 Halford Ave, <br />Santa Clara, CA - 95051 <br /> (408) 244-1160
             </span>
           </div>
-          <div className="pl-20">
+          <div className="pl-50">
           <div className="text-large-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
             {product_categories && product_categories?.length > 0 && (
               <div className="flex flex-col gap-y-2 pt-10 px-20">
