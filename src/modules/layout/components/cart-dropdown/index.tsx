c7058e05e7c19c9c8629cd4dc2dec199099687b2
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-09 10:15:10 +0530

    Update index.tsx - cart font

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -79,7 +79,7 @@ const CartDropdown = ({
       <Popover className="relative h-full">
         <Popover.Button className="h-full text-xl">
           <LocalizedClientLink
-            className="font-medium text-[#D35400] hover:text-black"
+            className="text-[#D35400] hover:text-black"
             href="/cart"
           >{`Cart (${totalItems})`}</LocalizedClientLink>
         </Popover.Button>
