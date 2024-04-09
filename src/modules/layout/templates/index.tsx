169d8923edca56bad32baa2bef90e2decdcdda9c
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-10 00:55:11 +0530

    Update index.tsx - updated bg for everything ig

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
-    <div>
+    <div className="bg-orange-50">
       <Nav />
       <main className="relative">{children}</main>
       <Footer />
