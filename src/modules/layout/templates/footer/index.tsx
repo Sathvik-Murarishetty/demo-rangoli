1f6cad0141e2161e20c3956d5dc20fe301d71d09
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-09 11:08:15 +0530

    Update index.tsx - footer poppins

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -1,13 +1,11 @@
-import { Text, clx } from "@medusajs/ui"
-
-import { getCategoriesList, getCollectionsList } from "@lib/data"
-
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import MedusaCTA from "@modules/layout/components/medusa-cta"
+import { Text, clx } from "@medusajs/ui";
+import { getCategoriesList, getCollectionsList } from "@lib/data";
+import LocalizedClientLink from "@modules/common/components/localized-client-link";
+import MedusaCTA from "@modules/layout/components/medusa-cta";
 
 export default async function Footer() {
-  const { collections } = await getCollectionsList(0, 6)
-  const { product_categories } = await getCategoriesList(0, 6)
+  const { collections } = await getCollectionsList(0, 6);
+  const { product_categories } = await getCategoriesList(0, 6);
 
   return (
     <footer className="border-t border-ui-border-base w-full bg-[#D35400]">
@@ -16,24 +14,24 @@ export default async function Footer() {
           <div>
             <LocalizedClientLink
               href="/"
-              className="text-3xl-semi text-black hover:text-ui-fg-base uppercase"
+              className="text-3xl-semi text-black hover:text-ui-fg-base uppercase poppins-semibold"
             >
               Rangoli Sweets
             </LocalizedClientLink>
-            <span className="text-large-regular text-ui-fg-subtle">
+            <span className="text-large-regular text-ui-fg-subtle poppins-regular">
                 <br />  1584 Halford Ave, <br />Santa Clara, CA - 95051 <br /> (408) 244-1160
             </span>
           </div>
           <div className="text-large-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
             {product_categories && product_categories?.length > 0 && (
               <div className="flex flex-col gap-y-2">
-                <span className="text-2xl txt-ui-fg-base">
+                <span className="text-2xl txt-ui-fg-base poppins-bold">
                   Categories
                 </span>
                 <ul className="text-large-regular grid grid-cols-1 gap-2">
                   {product_categories?.slice(0, 6).map((c) => {
                     if (c.parent_category) {
-                      return
+                      return null;
                     }
 
                     const children =
@@ -41,7 +39,7 @@ export default async function Footer() {
                         name: child.name,
                         handle: child.handle,
                         id: child.id,
-                      })) || null
+                      })) || null;
 
                     return (
                       <li
@@ -73,14 +71,14 @@ export default async function Footer() {
                           </ul>
                         )}
                       </li>
-                    )
+                    );
                   })}
                 </ul>
               </div>
             )}
             {collections && collections.length > 0 && (
               <div className="flex flex-col gap-y-2">
-                <span className="text-xl text-black">
+                <span className="text-xl text-black poppins-medium">
                   Categories
                 </span>
                 <ul
@@ -105,14 +103,14 @@ export default async function Footer() {
               </div>
             )}
             <div className="flex flex-col gap-y-2">
-              <span className="text-xl txt-ui-fg-base">Rangoli Sweets</span>
+              <span className="text-xl txt-ui-fg-base poppins-medium">Rangoli Sweets</span>
               <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle text-large-regular">
                 <li>
                   <a
                     href="/"
                     target="_blank"
                     rel="noreferrer"
-                    className="hover:text-ui-fg-base"
+                    className="hover:text-ui-fg-base poppins-regular"
                   >
                     Terms & Conditions
                   </a>
@@ -122,7 +120,7 @@ export default async function Footer() {
                     href="/"
                     target="_blank"
                     rel="noreferrer"
-                    className="hover:text-ui-fg-base"
+                    className="hover:text-ui-fg-base poppins-regular"
                   >
                     Customer Care
                   </a>
@@ -132,7 +130,7 @@ export default async function Footer() {
                     href="/"
                     target="_blank"
                     rel="noreferrer"
-                    className="hover:text-ui-fg-base"
+                    className="hover:text-ui-fg-base poppins-regular"
                   >
                     Contact Us
                   </a>
@@ -142,11 +140,11 @@ export default async function Footer() {
           </div>
         </div>
         <div className="flex w-full mb-16 justify-between text-ui-fg-subtle">
-          <Text className="txt-compact-small">
+          <Text className="txt-compact-small poppins-regular">
             © {new Date().getFullYear()} Rangoli Sweets. All rights reserved.
           </Text>
         </div>
       </div>
     </footer>
-  )
+  );
 }
