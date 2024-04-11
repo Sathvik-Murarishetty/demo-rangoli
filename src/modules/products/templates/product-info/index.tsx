8d1981c289062957b719a2f3f63fc1ab6733f58b
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-11 11:27:31 +0530

    Update index.tsx - category based tetx

--- src/modules/products/templates/product-info/index.tsx
+++ src/modules/products/templates/product-info/index.tsx
@@ -1,12 +1,30 @@
-import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
-import { Heading, Text } from "@medusajs/ui"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import { PricedProduct } from "@medusajs/medusa/dist/types/pricing";
+import { Heading, Text } from "@medusajs/ui";
+import LocalizedClientLink from "@modules/common/components/localized-client-link";
 
 type ProductInfoProps = {
-  product: PricedProduct
-}
+  product: PricedProduct;
+};
 
 const ProductInfo = ({ product }: ProductInfoProps) => {
+  // Function to get the unique text based on the collection title
+  const getUniqueText = (collectionTitle: string) => {
+    switch (collectionTitle) {
+      case "Sweets":
+        return "per 1 lb";
+      case "Beverages":
+        return "per 1 Pc";
+      // Add cases for other collection titles as needed
+      default:
+        return ""; // Default text if collection title doesn't match any case
+    }
+  };
+
+  // Get the unique text based on the collection title
+  const uniqueText = product.collection
+    ? getUniqueText(product.collection.title)
+    : "";
+
   return (
     <div id="product-info">
       <div className="flex flex-col gap-y-4 lg:max-w-[500px] mx-auto">
@@ -21,13 +39,14 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
         <Heading level="h2" className="text-3xl leading-10 text-ui-fg-base">
           {product.title}
         </Heading>
-
+        {/* Display unique text based on the collection title */}
+        <Text className="text-medium text-ui-fg-subtle">{uniqueText}</Text>
         <Text className="text-medium text-ui-fg-subtle">
           {product.description}
         </Text>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
