c05c7b20a34829bf9a84c3bec854f2e1da7c6f3a
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-11 17:01:37 +0530

    Update index.tsx - achevements error

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -8,7 +8,19 @@ import Autoplay from 'embla-carousel-autoplay';
 import { ChevronLeft, ChevronRight, ShoppingCart } from "lucide-react";
 import dynamic from "next/dynamic";
 
-const AnimatedNumbers = dynamic(
+interface AnimatedNumbersProps {
+  includeComma: boolean;
+  animateToNumber: number;
+  locale: string;
+  className: string;
+  configs: (index: number) => {
+    mass: number;
+    friction: number;
+    tensions: number;
+  };
+}
+
+const AnimatedNumbers = dynamic<AnimatedNumbersProps>(
   () => {
     return import("react-animated-numbers");
   },
@@ -36,6 +48,7 @@ const achievementsList = [
     postfix: "+",
   },
 ];
+
 const slides = [
   {
     image: 'https://res.cloudinary.com/dg0rdc0bd/image/upload/v1711485312/1_dkgpc1.png',
@@ -236,33 +249,22 @@ const goToSlide = (slideIndex: number) => {
 
   <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-            >
-              <h2 className="text-white text-4xl font-bold flex flex-row">
-                {achievement.prefix}
-                <AnimatedNumbers
-                  includeComma
-                  animateToNumber={parseInt(achievement.value)}
-                  locale="en-US"
-                  className="text-white text-4xl font-bold"
-                  configs={(_, index) => {
-                    return {
-                      mass: 1,
-                      friction: 100,
-                      tensions: 140 * (index + 1),
-                    };
-                  }}
-                />
-                {achievement.postfix}
-              </h2>
-              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-            </div>
-          );
-        })}
+        {achievementsList.map((achievement, index) => (
+        <AnimatedNumbers
+          key={index}
+          includeComma
+          animateToNumber={parseInt(achievement.value)}
+          locale="en-US"
+          className="text-white text-4xl font-bold"
+          configs={(index) => ({
+            mass: 1,
+            friction: 100,
+            tensions: 140 * (index + 1),
+          })}
+        />
+      ))}
+    </>
+  );
       </div>
     </div>
   
