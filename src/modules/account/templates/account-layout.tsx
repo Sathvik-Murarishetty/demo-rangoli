a0270f45acccb508c29c053a4d30994a9636f1b2
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-11 10:33:02 +0530

    Update account-layout.tsx

--- src/modules/account/templates/account-layout.tsx
+++ src/modules/account/templates/account-layout.tsx
@@ -16,7 +16,7 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
 }) => {
   return (
     <div className="bg-orange-50 m-0 flex-1 small:py-12">
-      <div className="flex-1 content-container h-full max-w-5xl mx-auto bg-white flex flex-col">
+      <div className="flex-1 content-container h-full max-w-5xl mx-auto bg-orange-50 flex flex-col">
         <div className="grid grid-cols-1  small:grid-cols-[240px_1fr] py-12">
           <div>{customer && <AccountNav customer={customer} />}</div>
           <div className="flex-1">{children}</div>
