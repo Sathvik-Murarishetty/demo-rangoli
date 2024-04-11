d90f4e3bbdae44381008aa7c2980a65bac595d8d
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-11 10:41:50 +0530

    Update index.tsx - bg white to orange 50

--- src/modules/account/components/order-card/index.tsx
+++ src/modules/account/components/order-card/index.tsx
@@ -22,7 +22,7 @@ const OrderCard = ({ order }: OrderCardProps) => {
   }, [order])
 
   return (
-    <div className="bg-white flex flex-col">
+    <div className="bg-orange-50 flex flex-col">
       <div className="uppercase text-large-semi mb-1">#{order.display_id}</div>
       <div className="flex items-center divide-x divide-gray-200 text-small-regular text-ui-fg-base">
         <span className="pr-2">
