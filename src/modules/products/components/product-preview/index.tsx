6c9bc5a80fcdef853d9203b7348c15809e781cda
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-13 08:36:45 +0530

    Update index.tsx - product preview card

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -44,13 +44,12 @@ export default async function ProductPreview({
             size="square"
             isFeatured={isFeatured}
           />
-          <div className="flex txt-compact-medium mt-4 justify-between">
-            <Text className="text-ui-fg-subtle">{productPreview.title}</Text>
-            <div className="flex items-center gap-x-2">
-              {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
-            </div>
-          </div>
-        </div>
+<div className="flex txt-compact-medium mt-4 justify-between flex-col">
+  <Text className="text-ui-fg-subtle text-large">{productPreview.title}</Text>
+  <div className="flex items-center gap-x-2">
+    {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+  </div>
+</div>
       </LocalizedClientLink>
     </div>
   )
