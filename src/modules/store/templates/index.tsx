02659dfc8f791cb3d6128adc223fd51764aee52b
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-13 08:25:34 +0530

    Update index.tsx - store featured products

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -7,7 +7,60 @@ import RefinementList from "@modules/store/components/refinement-list"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import React, { useCallback, useEffect, useState } from "react";
 
+
 import PaginatedProducts from "./paginated-products"
+import { Product } from "@medusajs/medusa"
+import { Metadata } from "next"
+
+import { getCollectionsList, getProductsList, getRegion } from "@lib/data"
+import FeaturedProducts from "@modules/home/components/featured-products"
+import Hero from "@modules/home/components/hero"
+import { ProductCollectionWithPreviews } from "types/global"
+import { cache } from "react"
+
+
+const getCollectionsWithProducts = cache(
+  async (
+    countryCode: string
+  ): Promise<ProductCollectionWithPreviews[] | null> => {
+    const { collections } = await getCollectionsList(0, 3)
+
+    if (!collections) {
+      return null
+    }
+
+    const collectionIds = collections.map((collection) => collection.id)
+
+    await Promise.all(
+      collectionIds.map((id) =>
+        getProductsList({
+          queryParams: { collection_id: [id] },
+          countryCode,
+        })
+      )
+    ).then((responses) =>
+      responses.forEach(({ response, queryParams }) => {
+        let collection
+
+        if (collections) {
+          collection = collections.find(
+            (collection) => collection.id === queryParams?.collection_id?.[0]
+          )
+        }
+
+        if (!collection) {
+          return
+        }
+
+        collection.products = response.products as unknown as Product[]
+      })
+    )
+
+    return collections as unknown as ProductCollectionWithPreviews[]
+  }
+)
+
+
 
 const StoreTemplate = ({
   sortBy,
@@ -55,24 +108,21 @@ const links = [
     setCurrentSlide(slideIndex);
   };
 
+  const collections = await getCollectionsWithProducts(countryCode)
+  const region = await getRegion(countryCode)
+
+  if (!collections || !region) {
+    return null
+  }
+
   return (
     <div className="bg-orange-50">
-      <div className="flex flex-col small:flex-row small:items-start py-6 content-container">
-      <RefinementList sortBy={sortBy || "created_at"} />
-      <div className="w-full">
-        <div className="mb-8 text-2xl-semi">
-          <h1>All products</h1>
-        </div>
-        <Suspense fallback={<SkeletonProductGrid />}>
-          <PaginatedProducts
-            sortBy={sortBy || "created_at"}
-            page={pageNumber}
-            countryCode={countryCode}
-          />
-        </Suspense>
+      <div className="py-12">
+        <ul className="flex flex-col gap-x-6">
+          <FeaturedProducts collections={collections} region={region} />
+        </ul>
       </div>
     </div>
-  </div>
       
   )
 }
