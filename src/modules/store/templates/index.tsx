453545a72536a543329dbe74603e80b72c3879ae
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-13 14:23:36 +0530

    Update index.tsx - border padding

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -64,10 +64,9 @@ const StoreTemplate = ({
     </div>
     <div className="flex justify-center py-10">
       <div className="flex justify-between gap-4" style={{ width: '90%' }}>
-        <div className="w-full m-2 border border-orange-400" style={{ width: '30%' }}>
-          <div className="py-10">
+        <div className="py-10">
+          <div className="w-full m-2 border border-orange-400" style={{ width: '30%' }}>
             <h1 className="mt-4 text-3xl font-poppins-bold text-gray-800">Categories</h1>
-          </div>
           <div>
             <ul className="text-xl font-poppins-bold text-gray-800 pl-5">
               <li>Bengali Sweets</li>
@@ -77,6 +76,7 @@ const StoreTemplate = ({
               <li>Namkeen</li>
               <li>Snacks</li>
             </ul>
+          </div>
           </div>
         </div>
         <div style={{ width: '70%' }}>
