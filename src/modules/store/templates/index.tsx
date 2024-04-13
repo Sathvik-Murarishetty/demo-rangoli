1b4ef13aaf08dd345873ff175b1dcc4ebd871dba
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-13 10:23:22 +0530

    Update index.tsx - store image

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -59,6 +59,9 @@ const links = [
 
   return (
     <div className="bg-orange-50">
+      <div className="h-40vw w-screen overflow-hidden">
+        <img src="https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712983839/Black_Elegant_Minimalist_Profile_LinkedIn_Banner_seha2o.png" alt="Your Image" className="w-full h-full object-cover" />
+      </div>
   <div className="flex justify-center py-10">
 <h1 className="mt-4 text-5xl font-cormorant-garamond-bold text-gray-800">Categories</h1>
 </div>
