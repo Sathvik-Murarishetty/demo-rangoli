daf0aa62b1da4e6bff720c77e38eb7813fd906d8
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-14 10:40:48 +0530

    Update index.tsx - countryCode

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -34,7 +34,7 @@ export default function ProductPreview({
       await addToCart({
         variantId,
         quantity: 1,
-        countryCode: region.country_code,
+        countryCode: region.countryCode,
       });
     }
   };
