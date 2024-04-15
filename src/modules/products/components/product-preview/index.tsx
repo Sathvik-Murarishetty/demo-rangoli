ea5647ea897ecd88c93ad2538cc9db2861d6e106
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-15 09:32:37 +0530

    Update index.tsx - og product preview

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,128 +1,55 @@
-"use client";
+import { Text } from "@medusajs/ui"
 
-import { useState, useEffect } from "react";
-import { Text, Button } from "@medusajs/ui";
-import { ProductPreviewType } from "types/global";
-import { retrievePricedProductById } from "@lib/data";
-import { getProductPrice } from "@lib/util/get-product-price";
-import { Region } from "@medusajs/medusa";
-import LocalizedClientLink from "@modules/common/components/localized-client-link";
-import Thumbnail from "../thumbnail";
-import PreviewPrice from "./price";
-import { addToCart } from "@modules/cart/actions";
-import { useParams } from "next/navigation"; // Adding this line
+import { ProductPreviewType } from "types/global"
+
+import { retrievePricedProductById } from "@lib/data"
+import { getProductPrice } from "@lib/util/get-product-price"
+import { Region } from "@medusajs/medusa"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import Thumbnail from "../thumbnail"
+import PreviewPrice from "./price"
 
 export default async function ProductPreview({
   productPreview,
   isFeatured,
   region,
 }: {
-  productPreview: ProductPreviewType;
-  isFeatured?: boolean;
-  region: Region;
+  productPreview: ProductPreviewType
+  isFeatured?: boolean
+  region: Region
 }) {
-  const [isAdding, setIsAdding] = useState(false);
-  const [pricedProduct, setPricedProduct] = useState<any>(null);
-  const [cheapestPrice, setCheapestPrice] = useState<any>(null);
-  const [dataFetched, setDataFetched] = useState(false); // State to track if data is fetched
-  const countryCode = useParams().countryCode as string; // Extracting country code
-
-  const handleAddToCart = async () => {
-    if (pricedProduct && pricedProduct.variants.length === 1) {
-      const variant = pricedProduct.variants[0];
-      setIsAdding(true);
-
-      await addToCart({
-        variantId: variant.id,
-        quantity: 1,
-        countryCode,
-      });
-
-      setIsAdding(false);
-    }
-  };
-
-  useEffect(() => {
-    if (!dataFetched) { // Check if data is not already fetched
-      const fetchData = async () => {
-        const fetchedProduct = await retrievePricedProductById({
-          id: productPreview.id,
-          regionId: region.id,
-        });
-
-        if (fetchedProduct) {
-          const { cheapestPrice } = getProductPrice({
-            product: fetchedProduct,
-            region,
-          });
-
-          setPricedProduct(fetchedProduct);
-          setCheapestPrice(cheapestPrice);
-          setDataFetched(true); // Set dataFetched to true after fetching data
-        }
-      };
-
-      fetchData();
-    }
-  }, [productPreview, region, dataFetched]);
+  const pricedProduct = await retrievePricedProductById({
+    id: productPreview.id,
+    regionId: region.id,
+  }).then((product) => product)
 
   if (!pricedProduct) {
-    return null;
+    return null
   }
 
+  const { cheapestPrice } = getProductPrice({
+    product: pricedProduct,
+    region,
+  })
+
   return (
-    <>
-      <div className="border rounded shadow p-2 flex flex-col h-full">
-        <LocalizedClientLink
-          href={`/products/${productPreview.handle}`}
-          className="group"
-        >
+    <LocalizedClientLink
+      href={`/products/${productPreview.handle}`}
+      className="group"
+    >
+      <div>
         <Thumbnail
           thumbnail={productPreview.thumbnail}
-          size="square"
+          size="full"
           isFeatured={isFeatured}
         />
-        <div className="flex-grow flex justify-between flex-col">
-          <div className="flex txt-compact-medium mt-4 justify-between flex-col">
-            <Text className="text-ui-fg-subtle text-large">
-              {productPreview.title}
-            </Text>
-            <div className="flex items-center gap-x-2">
-              {pricedProduct.variants.length === 1 && (
-                <Text className="text-gray-600">
-                  {pricedProduct.variants[0].title}
-                </Text>
-              )}
-              <div>
-                {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
-              </div>
-            </div>
+        <div className="flex txt-compact-medium mt-4 justify-between">
+          <Text className="text-ui-fg-subtle">{productPreview.title}</Text>
+          <div className="flex items-center gap-x-2">
+            {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
           </div>
         </div>
-              </LocalizedClientLink>
-                  {pricedProduct.variants.length !== 1 && (
-            <Button
-              variant="primary"
-              className="w-24 h-10 self-end mt-auto"
-            >
-              View Item
-            </Button>
-          )}
-          {pricedProduct.variants.length === 1 && (
-            <Button
-              variant="primary"
-              className="w-24 h-10 self-end mt-auto"
-              onClick={(event) => {
-    event.preventDefault(); // Prevent the default behavior of the link
-    handleAddToCart(); // Call the handleAddToCart function
-  }}
-              disabled={isAdding}
-            >
-              {isAdding ? "Adding..." : "Add to Cart"}
-            </Button>
-          )}
       </div>
-
-    </>
-  );
+    </LocalizedClientLink>
+  )
 }
