fd39d17b7ca64bd0413c6a51fde7c5f47b56550a
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-15 15:45:52 +0530

    Update index.tsx - product variant vierw

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,55 +1,80 @@
-import { Text } from "@medusajs/ui"
-
-import { ProductPreviewType } from "types/global"
-
-import { retrievePricedProductById } from "@lib/data"
-import { getProductPrice } from "@lib/util/get-product-price"
-import { Region } from "@medusajs/medusa"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import Thumbnail from "../thumbnail"
-import PreviewPrice from "./price"
+import { Text, Button } from "@medusajs/ui";
+import { ProductPreviewType } from "types/global";
+import { retrievePricedProductById } from "@lib/data";
+import { getProductPrice } from "@lib/util/get-product-price";
+import { Region } from "@medusajs/medusa";
+import LocalizedClientLink from "@modules/common/components/localized-client-link";
+import Thumbnail from "../thumbnail";
+import PreviewPrice from "./price";
 
 export default async function ProductPreview({
   productPreview,
   isFeatured,
   region,
 }: {
-  productPreview: ProductPreviewType
-  isFeatured?: boolean
-  region: Region
+  productPreview: ProductPreviewType;
+  isFeatured?: boolean;
+  region: Region;
 }) {
   const pricedProduct = await retrievePricedProductById({
     id: productPreview.id,
     regionId: region.id,
-  }).then((product) => product)
+  }).then((product) => product);
 
   if (!pricedProduct) {
-    return null
+    return null;
   }
 
   const { cheapestPrice } = getProductPrice({
     product: pricedProduct,
     region,
-  })
+  });
+
+  // Check if there's only one variant
+  const hasSingleVariant = pricedProduct.variants.length === 1;
 
   return (
     <LocalizedClientLink
       href={`/products/${productPreview.handle}`}
       className="group"
     >
-      <div>
+      <div className="border rounded shadow p-2 flex flex-col h-full">
         <Thumbnail
           thumbnail={productPreview.thumbnail}
-          size="full"
+          size="square"
           isFeatured={isFeatured}
         />
-        <div className="flex txt-compact-medium mt-4 justify-between">
-          <Text className="text-ui-fg-subtle">{productPreview.title}</Text>
-          <div className="flex items-center gap-x-2">
-            {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+        <div className="flex-grow flex justify-between flex-col">
+          <div className="flex txt-compact-medium mt-4 justify-between flex-col">
+            <Text className="text-ui-fg-subtle text-large">
+              {productPreview.title}
+            </Text>
+            <div className="flex items-center gap-x-2">
+              {hasSingleVariant && (
+                <Text className="text-gray-600">
+                  {pricedProduct.variants[0].title}
+                </Text>
+              )}
+              <div>
+                {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+              </div>
+            </div>
           </div>
+          {!hasSingleVariant && (
+            <Button
+              variant="primary"
+              className="w-24 h-10 self-end mt-auto"
+            >
+              View Item
+            </Button>
+          )}
+          {hasSingleVariant && (
+            <Button variant="primary" className="w-24 h-10 self-end mt-auto">
+              Add to Cart
+            </Button>
+          )}
         </div>
       </div>
     </LocalizedClientLink>
-  )
+  );
 }
