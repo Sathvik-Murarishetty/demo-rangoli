eba0ab596525686548b9204e5b056b57d0b4ff09
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-20 00:14:05 +0530

    Update index.tsx - product rail

--- src/modules/home/components/featured-products/product-rail/index.tsx
+++ src/modules/home/components/featured-products/product-rail/index.tsx
@@ -1,23 +1,25 @@
-import { Region } from "@medusajs/medusa"
-import { Text } from "@medusajs/ui"
-
-import InteractiveLink from "@modules/common/components/interactive-link"
-import ProductPreview from "@modules/products/components/product-preview"
-import { ProductCollectionWithPreviews } from "types/global"
-import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
+import React from 'react';
+import { Region } from '@medusajs/medusa';
+import { Text } from '@medusajs/ui';
+import InteractiveLink from '@modules/common/components/interactive-link';
+import ProductPreview from '@modules/products/components/product-preview';
+import { ProductCollectionWithPreviews } from 'types/global';
 
 export default function ProductRail({
   collection,
   region,
 }: {
-  collection: ProductCollectionWithPreviews
-  region: Region
+  collection: ProductCollectionWithPreviews;
+  region: Region;
 }) {
-  const { products } = collection
+  const { products } = collection;
 
   if (!products) {
-    return null
+    return null;
   }
+
+  // Extract only the first three products from the collection
+  const firstThreeProducts = products.slice(0, 3);
 
   return (
     <div className="content-container py-12 small:py-24">
@@ -28,17 +30,12 @@ export default function ProductRail({
         </InteractiveLink>
       </div>
       <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
-        {products &&
-          products.map((product) => (
-            <li key={product.id}>
-              <ProductPreview
-                productPreview={product}
-                region={region}
-                isFeatured
-              />
-            </li>
-          ))}
+        {firstThreeProducts.map((product) => (
+          <li key={product.id}>
+            <ProductPreview productPreview={product} region={region} isFeatured />
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
