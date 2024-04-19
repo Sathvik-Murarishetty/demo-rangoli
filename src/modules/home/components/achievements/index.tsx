0abe6599c49aa63b7109a933c9d8f1bc3bd27d47
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-19 10:21:52 +0530

    Update index.tsx - achievements

--- src/modules/home/components/achievements/index.tsx
+++ src/modules/home/components/achievements/index.tsx
@@ -2,61 +2,61 @@ import React from "react";
 import dynamic from "next/dynamic";
 
 const AnimatedNumbers = dynamic(
-  () => {
-    return import("react-animated-numbers");
-  },
-  { ssr: false }
+    () => {
+        return import("react-animated-numbers");
+    },
+    { ssr: false }
 );
 
 const achievementsList = [
-  {
-    metric: "Delicious Dinners",
-    value: "5245",
-    postfix: "+",
-  },
-  {
-    metric: "Experienced Chef",
-    value: "60",
-    postfix: "+",
-  },
-  {
-    metric: "Our Awards",
-    value: "1359",
-    postfix: "+",
-  },
-  {
-    metric: "Happy Customers",
-    value: "6560",
-    postfix: "+",
-  },
+    {
+        metric: "Delicious Dinners",
+        value: "5245",
+        postfix: "+",
+    },
+    {
+        metric: "Experienced Chef",
+        value: "60",
+        postfix: "+",
+    },
+    {
+        metric: "Our Awards",
+        value: "1359",
+        postfix: "+",
+    },
+    {
+        metric: "Happy Customers",
+        value: "6560",
+        postfix: "+",
+    },
 ];
 
 const AchievementsSection = () => {
-  return (
-    <div className="pb-8 px-20 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-            >
-              <h2 className="text-[#D35400] text-4xl font-bold flex flex-row">
-                <AnimatedNumbers
-                  includeComma
-                  animateToNumber={parseInt(achievement.value)}
-                  locale="en-US"
-                  className="text-[#D35400] text-4xl font-bold"
-                />
-                {achievement.postfix}
-              </h2>
-              <p className="text-black text-2xl"> {achievement.metric}</p>
+    return (
+        <div className="pb-8 px-20 xl:gap-16 sm:py-16 xl:px-16">
+            <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+                {achievementsList.map((achievement, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+                        >
+                            <h2 className="text-[#D35400] text-4xl font-bold flex flex-row">
+                                <AnimatedNumbers
+                                    includeComma
+                                    animateToNumber={parseInt(achievement.value)}
+                                    locale="en-US"
+                                    className="text-[#D35400] text-4xl font-bold"
+                                />
+                                {achievement.postfix}
+                            </h2>
+                            <p className="text-black text-2xl"> {achievement.metric}</p>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default AchievementsSection;
