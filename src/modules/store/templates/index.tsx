51ea5ee5ff1331d258312d79542d509905cc936c
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-19 10:27:38 +0530

    Update index.tsx - store template

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -1,22 +1,57 @@
 import { Suspense } from "react"
-
 import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
 import RefinementList from "@modules/store/components/refinement-list"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-import React, { useCallback, useEffect, useState } from "react";
-
-
 import PaginatedProducts from "./paginated-products"
-
 import { Product } from "@medusajs/medusa"
 import { Metadata } from "next"
-
 import { getCollectionsList, getProductsList, getRegion } from "@lib/data"
 import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import { ProductCollectionWithPreviews } from "types/global"
-import { cache } from "react"
+import { cache } from "react";
+import BestSellers from "@modules/home/components/best-sellers";
 
+const getCollectionsWithProducts = cache(
+  async (
+    countryCode: string
+  ): Promise<ProductCollectionWithPreviews[] | null> => {
+    const { collections } = await getCollectionsList(0, 3)
+
+    if (!collections) {
+      return null
+    }
+
+    const collectionIds = collections.map((collection) => collection.id)
+
+    await Promise.all(
+      collectionIds.map((id) =>
+        getProductsList({
+          queryParams: { collection_id: [id] },
+          countryCode,
+        })
+      )
+    ).then((responses) =>
+      responses.forEach(({ response, queryParams }) => {
+        let collection
+
+        if (collections) {
+          collection = collections.find(
+            (collection) => collection.id === queryParams?.collection_id?.[0]
+          )
+        }
+
+        if (!collection) {
+          return
+        }
+
+        collection.products = response.products as unknown as Product[]
+      })
+    )
+
+    return collections as unknown as ProductCollectionWithPreviews[]
+  }
+)
 
 const StoreTemplate = async ({
   sortBy,
@@ -27,43 +62,67 @@ const StoreTemplate = async ({
   page?: string
   countryCode: string
 }) => {
-  
- return (
-  <div className="bg-orange-50">
-    <div className="h-40vw w-screen overflow-hidden">
-      <img src="https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712983839/Black_Elegant_Minimalist_Profile_LinkedIn_Banner_seha2o.png" alt="Your Image" className="w-full h-full object-cover" />
-    </div>
-    <div className="flex justify-center py-10">
-      <div className="flex justify-between gap-4" style={{ width: '90%' }}>
-      <div className="pl-5 pt-10" style={{ width: '30%' }}>
-        <div className="w-full m-2">
-        <div className="pb-10 pl-5">
-            <h1 className="mt-4 text-3xl font-poppins-bold text-gray-800">Categories</h1>
+
+  const images = [
+    'https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712633012/WhatsApp_Image_2024-04-09_at_08.52.50_jslmza.jpg',
+    'https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712633012/WhatsApp_Image_2024-04-09_at_08.52.17_1_oczoxz.jpg',
+    'https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712633013/WhatsApp_Image_2024-04-09_at_08.52.16_tkxkq0.jpg',
+    'https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712633012/WhatsApp_Image_2024-04-09_at_08.52.17_wapqmc.jpg'
+  ];
+
+  const links = [
+    '/products/mysore-pak',
+    '/products/motichoor',
+    '/products/kalakand',
+    '/products/kaju-katlii'
+  ];
+
+  const collections = await getCollectionsWithProducts(countryCode)
+  const region = await getRegion(countryCode)
+
+  if (!collections || !region) {
+    return null
+  }
+
+  return (
+    <div className="bg-orange-50">
+      <div className="h-40vw w-screen overflow-hidden">
+        <img src="https://res.cloudinary.com/dg0rdc0bd/image/upload/v1712983839/Black_Elegant_Minimalist_Profile_LinkedIn_Banner_seha2o.png" alt="Your Image" className="w-full h-full object-cover" />
+      </div>
+      <div className="flex justify-center py-10">
+        <div className="flex justify-between gap-4" style={{ width: '90%' }}>
+          <div className="pl-5 pt-10" style={{ width: '30%' }}>
+            <div className="w-full m-2 sticky top-0">
+              <div className="pb-10 pl-5">
+                <h1 className="mt-4 text-3xl font-poppins-bold text-gray-800">Categories</h1>
+              </div>
+              <div>
+                <ul className="text-xl font-poppins-bold text-gray-800 pl-5 pb-10">
+                  <li>Bengali Sweets</li>
+                  <li>Bakery</li>
+                  <li>Beverages</li>
+                  <li>Sweets</li>
+                  <li>Namkeen</li>
+                  <li>Snacks</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div>
-            <ul className="text-xl font-poppins-bold text-gray-800 pl-5 pb-10">
-              <li>Bengali Sweets</li>
-              <li>Bakery</li>
-              <li>Beverages</li>
-              <li>Sweets</li>
-              <li>Namkeen</li>
-              <li>Snacks</li>
-            </ul>
-          </div>
-          </div>
-        </div>
-        <div style={{ width: '70%' }}>
-          <div className="flex justify-center py-10">
-            <h1 className="mt-4 text-5xl font-poppins-bold text-gray-800">Our Best Sellers</h1>
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+          <div style={{ width: '70%' }}>
+            <div className="flex justify-center py-10">
+              <h1 className="mt-4 text-5xl font-poppins-bold text-gray-800">Our Best Sellers</h1>
+            </div>
+            <BestSellers />
+            <div>
+              <ul className="flex flex-col gap-x-6">
+                <FeaturedProducts collections={collections} region={region} />
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
 }
-
 
 export default StoreTemplate
