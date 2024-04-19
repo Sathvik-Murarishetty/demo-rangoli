a24483436fa884cd29f8457ac361e68b4068f342
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-19 11:09:00 +0530

    Update index.tsx - product raik

--- src/modules/home/components/featured-products/product-rail/index.tsx
+++ src/modules/home/components/featured-products/product-rail/index.tsx
@@ -4,6 +4,7 @@ import { Text } from "@medusajs/ui"
 import InteractiveLink from "@modules/common/components/interactive-link"
 import ProductPreview from "@modules/products/components/product-preview"
 import { ProductCollectionWithPreviews } from "types/global"
+import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
 
 export default function ProductRail({
   collection,
@@ -19,14 +20,14 @@ export default function ProductRail({
   }
 
   return (
-    <div className="content-container py-6 small:py-10">
+    <div className="content-container py-12 small:py-24">
       <div className="flex justify-between mb-8">
         <Text className="txt-xlarge">{collection.title}</Text>
         <InteractiveLink href={`/collections/${collection.handle}`}>
           View all
         </InteractiveLink>
       </div>
-      <ul className="grid grid-cols-2 small:grid-cols-5 gap-x-6 gap-y-24 small:gap-y-36">
+      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
         {products &&
           products.map((product) => (
             <li key={product.id}>
