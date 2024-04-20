1f34542a1bd884fffbca57b98916b791bc5b6729
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-20 15:58:51 +0530

    Update index.tsx - corporate orders

--- src/modules/corporate-orders/templates/index.tsx
+++ src/modules/corporate-orders/templates/index.tsx
@@ -1,17 +1,14 @@
 const Corporateorder = () => {
     return (
         <div className="bg-orange-50">
-            <h1 className="mt-4 text-5xl flex justify-center font-cormorant-garamond-bold text-gray-800">Our Kitchen</h1>
-            <div className="flex flex-col items-center justify-center w-full px-10">
+            <h1 className="mt-4 text-5xl flex justify-center font-cormorant-garamond-bold text-gray-800">The TASTEMAKERS of California</h1>
+            <div className="flex flex-col items-center justify-center w-80vh px-10">
                 <p className="mt-6 text-lg text-gray-700">
-                    At Rangoli Sweets in Santa Clara, California, we celebrate the art of traditional Indian sweets with a commitment to purity in every bite. Our Experiential Commercial Kitchen, equipped with top-tier facilities, allows customers to witness the meticulous process behind our beloved delicacies. 
-                </p>
-                <p className="mt-6 text-lg text-gray-700">
-                    We uphold the highest quality standards, favoring natural ingredients over artificial additives. This dedication extends to our unique practice of producing our own milk, ensuring that each sweet is crafted with the freshest, most wholesome milk available. At Rangoli Sweets, safety and satisfaction go hand in hand, offering our customers a taste of authentic, safe, and delightful Indian tradition.
+                    At our Indian sweet shop in Santa Clara, California, we celebrate the timeless tradition of expressing love and appreciation through sweets. Each treat from our shop is more than just a delicacy; it's a symbol of joy and cultural heritage. We use only the finest ingredients and a perfect blend of traditional recipes and modern techniques to create sweets that are not just delicious, but also meaningful. Whether it's celebrating a special occasion or sharing a piece of our culture, our sweets are crafted to bring people together and make every moment sweeter.
                 </p>
             </div>
             <div className="flex flex-col items-center">
-              <h1 className="mt-8 text-5xl font-cormorant-garamond-bold text-gray-800">Our Clients</h1>
+              <h1 className="mt-8 text-5xl font-cormorant-garamond-bold text-gray-800">How it Works</h1>
               <div className="flex justify-between gap-x-10 py-10">
                 <img src="https://res.cloudinary.com/dg0rdc0bd/image/upload/v1713605625/amazon_wxv4ka.svg" alt="Brand 1" className="w-auto h-20" />
                 <img src="https://res.cloudinary.com/dg0rdc0bd/image/upload/v1713605625/apple_tzi1bw.svg" alt="Brand 2" className="w-auto h-20" />
