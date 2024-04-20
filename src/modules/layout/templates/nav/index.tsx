ca30c7e5335609cc4e33f13a2983512b10705867
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-21 02:55:14 +0530

    Update index.tsx - flex removed

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -11,11 +11,9 @@ export default async function Nav() {
   <div className="sticky top-0 inset-x-0 z-50 group">
    <header className="relative h-20 mx-auto duration-200 bg-orange-50 text-[#D35400] text-xl border-ui-border-base">
     <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
-     <div className="flex-1 basis-0 h-full flex items-center">
       <div className="h-full">
        <SideMenu regions={regions} />
       </div>
-     </div>
 
      <div className="flex items-center gap-x-10 h-full flex-1 basis-0 justify-start">
       <div className="hidden small:flex items-center gap-x-10 h-full">
