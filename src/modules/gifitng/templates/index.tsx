8db9890ebd8ebeffadbc4e8789875de99b18ca24
Sathvik-Murarishetty <96581785+Sathvik-Murarishetty@users.noreply.github.com>
2024-04-24 01:43:08 +0530

    Update index.tsx - font for heaidng

--- src/modules/gifitng/templates/index.tsx
+++ src/modules/gifitng/templates/index.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Gifting = () => {
     return (
         <div className="bg-orange-50">
-            <h1 className="pt-4 text-5xl flex justify-center font-cormorant-garamond-bold text-gray-800">MAKE YOUR WEDDING A SPECTACULAR EVENT</h1>
+            <h1 className="pt-4 text-5xl flex justify-center font-zcool-xiaowei-regular text-gray-800">MAKE YOUR EVENT SPECTACULAR</h1>
             <div className="flex justify-center py-20">
                 <div className="max-w-6xl w-full px-4">
                     <div className="grid grid-cols-1 gap-8">
                         <div>
                             <p className="mt-6 text-lg text-gray-700">
-                                A wedding is a vibrant celebration, brimming with festivities that transform it into an unforgettable extravaganza. Amidst this joyful atmosphere, the spirit of happiness flourishes. The giving of gifts and warm wishes are integral parts of the celebration, enhancing the festive ambiance. To complement the grandeur of your special day, we offer an exquisite selection of Wedding Hampers, designed to embody the emotion, colors, and jubilation of your magnificent occasion.
+                                An event is a vibrant celebration, brimming with festivities that transform it into an unforgettable extravaganza. Amidst this joyful atmosphere, the spirit of happiness flourishes. The giving of gifts and warm wishes are integral parts of the celebration, enhancing the festive ambiance. To complement the grandeur of your special day, we offer an exquisite selection of Hampers, designed to embody the emotion, colors, and jubilation of your magnificent occasion.
                             </p>
                         </div>
                     </div>
